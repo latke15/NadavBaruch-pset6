07649d5e2b17d0385c736a53e3375f72ae7f7087
latke15 <nadav_baruch@hotmail.com>
2016-12-06 16:23:42 +0100

    iets meer

--- NadavBaruch-pset6/ViewController.swift
+++ NadavBaruch-pset6/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var countryCodeInput: UITextField!
+    @IBOutlet weak var cityInput: UITextField!
+    
+    let myJson = String()
+//    var countryCode = String()
+//    var city = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,55 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func checkShabbat(_ sender: Any) {
+        if countryCodeInput.text == "" || cityInput.text == "" {
+            showAlertView(title: "Attention!", withDescription: "You forgot your input!", buttonText: "Understood!")
+        }
+        
+        let countryCode = countryCodeInput.text
+        let city = cityInput.text
+        
+        let url = URL(string: "https://www.hebcal.com/shabbat/?cfg=json&city=" + countryCode + "-" + city + "&m=50")
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard error == nil else {
+                self.showAlertView(title:"Attention!", withDescription:"Error occured!", buttonText:"Understood!")
+                print("error!")
+                return
+            }
+            guard let data = data else {
+                self.showAlertView(title:"Attention!", withDescription:"No data could be found!", buttonText:"Understood!")
+                print("Data is empty")
+                return
+            }
+            
+            // Get status code
+            let httpResponse = response as! HTTPURLResponse
+            if httpResponse.statusCode == 400{
+                self.showAlertView(title:"Attention!", withDescription:"Bad request, please contact the administrator.", buttonText:"Understood!")
+            }
+            if httpResponse.statusCode == 500{
+                self.showAlertView(title:"Attention!", withDescription:"Internal server error, please contact the administrator.", buttonText:"Understood!")
+            }
+            
+            if httpResponse.statusCode == 200{
+                print("Succeed to maintain data!")
+            }
+            
+            
+            let dict = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            
+        }
+        task.resume()
+        self.loadView()
+    }
+    
+    // Show an alert
+    func showAlertView(title: String, withDescription description: String, buttonText text: String) {
+        let alertController = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: text, style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
 
 
